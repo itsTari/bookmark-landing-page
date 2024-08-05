33f0e2597ee3d9cec23629721e57bc14c7fecd15
itsTari <tariebisamuel16@gmail.com>
2024-08-05 11:19:03 +0100

    added to the Feature component

--- src/component/Features.jsx
+++ src/component/Features.jsx
@@ -1,15 +1,24 @@
 import SimpleBookmark from "./SimpleBookmark"
+import EasySharing from "./EasySharing"
+import SpeedySearching from "./SpeedySearching"
+import { useState } from "react"
+import { NavLink } from "react-router-dom"
 const Features = () => {
+    const [display, setDisplay]= useState('SimpleBookmark')
+    // const [active, setActive]= useState('border-b-2 border-red-500')
   return (
     <section className="flex items-center  flex-col gap-10 py-40" >
         <h1 className="text-3xl sm:text-5xl">Features</h1>
         <p className="w-1/2 text-center">our aim is to make it quick and easy for you to access your <br/> favourite websites. Your bookmark sync between your devices <br/> so you can access them on the go</p>
         <div className="flex gap-10 py-20">
-            <a className="border-b-2 border-red-500 hover:text-red-500 h-10 w-60 text-center shadow-md">Simple Bookmarking</a>
-            <a className="hover:text-red-500 h-10 w-60 text-center shadow-md">Speedy Searching</a>
-            <a className="hover:text-red-500 h-10 w-60 text-center shadow-md">Easy Sharing</a>
+            <button onClick={()=> setDisplay('SimpleBookmark')} className={display ? "border-b-2 border-red-500 hover:text-red-500 h-10 w-60 text-center shadow-md" :"hover:text-red-500 h-10 w-60 text-center shadow-md" }><a>Simple Bookmarking</a></button>
+            <button onClick={()=> setDisplay('SpeedySearching')} className="hover:text-red-500 h-10 w-60 text-center shadow-md"><a >Speedy Searching</a></button>
+            <button onClick={()=> setDisplay('EasySharing')} className="hover:text-red-500 h-10 w-60 text-center shadow-md"><a>Easy Sharing</a></button>
         </div>
-        <SimpleBookmark/>
+        {display === 'SimpleBookmark' && <SimpleBookmark/> }
+        {display === 'SpeedySearching' && <SpeedySearching/> }
+        {display === 'EasySharing' && <EasySharing/> }
+        
     </section>
   )
 }
---
+++ src/component/SpeedySearching.jsx
@@ -0,0 +1,22 @@
+import Illustrator from "../assets/images/illustration-features-tab-2.svg";
+const SpeedySearching = () => {
+  return (
+    <section className="flex items-center justify-between px-24">
+      <div className="w-1/2 relative">
+        {/* <div className='bg-blue-800 w-96 h-60 rounded-bl-3xl place-self-start absolute top-60 left-0 p-0'></div> */}
+        <img className="px-10" src={Illustrator}></img>
+      </div>
+      <div className="w-1/2 pl-24">
+        <h2 className="text-2xl sm:text-4xl ">Speed Searching</h2>
+        <p className="py-10">
+          Organize your bookmark however you like. Our simple drag-and-drop
+          interface gives you complate control over how you manager your
+          favorites sites{" "}
+        </p>
+        <button className="bg-blue-800 p-3 rounded">More Info</button>
+      </div>
+    </section>
+  )
+};
+
+export default SpeedySearching;
